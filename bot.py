2ca0b0b1c6daa8e3d099399e37b2c32c1200cb05
Alexander Yarasoff <alexander.yarasoff@yandex.ru>
2023-03-13 13:36:23 +0300

    Эхо бот

--- bot.py
+++ bot.py
@@ -2,17 +2,21 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
+from handlers import other_handlers, user_handlers
 
 
 # Функция конфигурирования и запуска бота
-async def main() -> None:
-
+async def main():
     # Загружаем конфиг в переменную config
     config: Config = load_config()
 
     # Инициализируем бот и диспетчер
     bot: Bot = Bot(token=config.tg_bot.token)
     dp: Dispatcher = Dispatcher()
+
+    # Регистриуем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
 
     # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
@@ -21,13 +25,3 @@ async def main() -> None:
 
 if __name__ == '__main__':
     asyncio.run(main())
-
-
-
-
-
-
-
-
-
-
